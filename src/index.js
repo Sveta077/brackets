f3e43644da16e663a1986b0d9b951fbb2e330f2d
Sveta077 <pugach.svetka@gmail.com>
2022-02-23 23:45:43 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig)
+{ let arr = [];
+  let result = true; 
+  for (let s of str.split('')) 
+  {
+    for (let e of bracketsConfig)
+    {
+        if (e[1]==s ) 
+        {   r=arr.slice(-1); 
+            if ( r != e[0] && e[0] !=s )  
+                    { 
+                      result= false; break;
+                    }  
+            if (r == e[0]) {arr.pop();break;} 
+         }   
+        if (e[0]==s) {arr.push(s); break} 
+    }  
+    if (!result) break;
+  }; 
+  return result && arr.length==0; 
 }
+
